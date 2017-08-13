edba84c8914be38d7759d618f2c6c3024ebd100b
Gabriel Martins Andrade <gabrielandrade21@hotmail.com>
2017-08-13 00:46:07 -0300

    Alterando arquivo de boas vindas

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-“Seja bem-vindo”
+<html>
+    <head>
+        <title>Testando PHP com Docker</title>
+    </head>
+    <body>
+        <p>
+            <?php
+                if (isset($_POST['submit'])) {
+                    $nome = $_POST['nome'];
+                    echo "Seja Bem-Vindo ".$nome;
+                }
+                else{
+                    echo '<form action="" method="post">
+                    <p>Digite seu nome:</p>
+                    <input name="nome" type="text" />
+                    <input name="submit" type="submit" />
+                </form>';
+                }
+            ?>
+        </p>
+    </body>
+</html>
